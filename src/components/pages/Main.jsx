b930281c20f866063877d1170c58dcfdd5da599e
Selim Gssim <selim.gssim@woodmark.de>
2023-10-08 18:46:16 +0200

    change currency

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -113,11 +113,11 @@ const Main = () => {
                               <label className="form-label">Price Range</label>
                               <select className="form-control">
                                 <option>Choose Price Range</option>
-                                <option>$0-1000</option>
-                                <option>$1000-$2000</option>
-                                <option>$2000-$3000</option>
-                                <option>$3000-$4000</option>
-                                <option>$4000-$5000</option>
+                                <option>TND 0-1000</option>
+                                <option>TND 1000-2000</option>
+                                <option>TND 2000-3000</option>
+                                <option>TND 3000-4000</option>
+                                <option>TND 4000-5000</option>
                               </select>
                             </div>
                           </div>
