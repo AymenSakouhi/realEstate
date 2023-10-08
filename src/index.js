d0c4a49fd623e3ec9ec1102eaae6157b68f16cf4
Aymen Sakouhi <a.sakouhi@reply.de>
2023-10-08 20:17:12 +0200

    adding react-router-dom config to the navbar and few pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Properties from "./components/pages/Properties";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <div className="App">
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/properties" element={<Properties />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </div>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
