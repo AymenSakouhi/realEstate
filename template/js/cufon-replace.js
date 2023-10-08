9c6f303d886ae40b0f249e70b2c297145d3c5132
Aymen Sakouhi <a.sakouhi@reply.de>
2023-10-08 17:06:52 +0200

    pushing new template

--- template/js/cufon-replace.js
+++
@@ -1,2 +0,0 @@
-Cufon.replace('#menu a, .nivo-caption strong, h2, h3, h4, .date, .call', { fontFamily: 'Didact Gothic', hover:true });
-
--- template/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-   // initiate tool tip
-	// basic usage  
-	$('.normaltip').aToolTip();  
-	
-	$('#form_1').jqTransform({imgPath:'jqtransformplugin/img/'});	
- });
